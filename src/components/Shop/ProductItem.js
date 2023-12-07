c197dcacb7e06660048fdbe06ec2ad817834667b
Hrishikesh <hrishikesh.joshi1997@gmail.com>
2023-12-07 17:00:53 +0530

    Managed Products and Cart Items using cartSlice

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,15 @@
+import { useDispatch } from "react-redux";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
+import { cartActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { title, price, description, id } = props;
+  const dispatch = useDispatch();
+
+  const addToCartHandle = () => {
+    dispatch(cartActions.addItemToCart({ title, price, id }));
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +20,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandle}>Add to Cart</button>
         </div>
       </Card>
     </li>
